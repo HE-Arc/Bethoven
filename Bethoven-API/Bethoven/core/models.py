a653bfb3223d72e4472a446635edcfe3a62e54d7
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-02-23 23:12:41 +0100

    add following mecanic

--- Bethoven-API/Bethoven/core/models.py
+++ Bethoven-API/Bethoven/core/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class BethovenUser(models.Model):
-    """This model extends the User basic authentication model, while the economy field"""
+    """Model linked to the User basic authentication model, while the economy field"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     coins = models.IntegerField
+    
+    # many-to-many in itself, needs to be asymetrical
+    following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+    #Gives access to
+        #following = user.following.all()
+        #followers = user.followers.all()
