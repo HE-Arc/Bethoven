31672352429e95153005a873e6b31f338dbc5b3e
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-02-23 22:27:35 +0100

    add user model based on django auth

--- Bethoven-API/Bethoven/core/models.py
+++ Bethoven-API/Bethoven/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BethovenUser(models.Model):
+    """This model extends the User basic authentication model, while the economy field"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    coins = models.IntegerField
