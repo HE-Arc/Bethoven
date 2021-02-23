be57224de9b72b296e85871fe67a200145d5e951
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-02-24 00:17:14 +0100

    add bet with owner

--- Bethoven-API/Bethoven/core/models.py
+++ Bethoven-API/Bethoven/core/models.py
@@ -6,10 +6,23 @@ from django.contrib.auth.models import User
 class BethovenUser(models.Model):
     """Model linked to the User basic authentication model, while the economy field"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    coins = models.IntegerField
-    
+    coins = models.IntegerField()
+
     # many-to-many in itself, needs to be asymetrical
     following = models.ManyToManyField('self', related_name='followers', symmetrical=False)
     #Gives access to
         #following = user.following.all()
         #followers = user.followers.all()
+
+class Bet(models.Model):
+    """Model that represent a bet with its description, choices, and closure mecanism"""
+    #Bet gestion
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    choice1 = models.CharField(max_length=50)    
+    choice2 = models.CharField(max_length=50)
+    #Closure gestion
+    isClosed = models.BooleanField(default=False)
+    result = models.IntegerField(blank=True, null=True)
+    #"Own" relationship with user. Nullable as we dont want the bets to be destroyed on user deletion
+    owner = models.ForeignKey(BethovenUser, related_name='Bets', on_delete=models.SET_NULL, null=True)
