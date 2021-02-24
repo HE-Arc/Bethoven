46aae1e522c0260444eb16d94868d546269ce407
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-02-24 12:11:27 +0100

    try to put bethoven as installed app, cant make migration

--- Bethoven-API/Bethoven/core/models.py
+++ Bethoven-API/Bethoven/core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 #Timestamped abstract model
 class TimeStampedModel(models.Model):
     """Abstract model that add the timestamps created_at and updated_at"""
