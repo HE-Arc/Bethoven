82c976d73a0cf208646720de43f1cd4bcb7a0ecb
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-08 18:13:26 +0100

    remove register from views

--- Bethoven-API/Bethoven/main_app/views.py
+++ Bethoven-API/Bethoven/main_app/views.py
@@ -7,23 +7,6 @@ from .api.serializers import *
 from rest_framework import generics, permissions, mixins
 from rest_framework.response import Response
 from .models import *
-from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
-
-
-   
-   
-########### register here #####################################  
-class Register(generics.GenericAPIView):
-    serializer_class = RegisterSerializer
-    permission_classes = [permissions.AllowAny]
-    def post(self, request, *args,  **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        bethovenUser = serializer.save()
-        return Response({
-            "user": BethovenUserSerializers(bethovenUser,context=self.get_serializer_context()).data,
-            "message": "User Created Successfully.  Now perform Login to get your token",
-        })
 
 class ServerState(generics.GenericAPIView):
     def get(self, request, *args,  **kwargs):
