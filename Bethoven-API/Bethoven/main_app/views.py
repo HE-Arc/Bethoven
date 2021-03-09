577ee66843dad51c025f741e78f8b0c3ad7edf75
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-09 23:50:55 +0100

    simplify views

--- Bethoven-API/Bethoven/main_app/views.py
+++ Bethoven-API/Bethoven/main_app/views.py
@@ -1,12 +1,5 @@
-from django.shortcuts import render, redirect 
-from django.contrib import messages 
-from django.contrib.auth import authenticate, login 
-from django.contrib.auth.decorators import login_required 
-from django.contrib.auth.forms import AuthenticationForm 
-from .api.serializers import *
-from rest_framework import generics, permissions, mixins
+from rest_framework import generics
 from rest_framework.response import Response
-from .models import *
 
 class ServerState(generics.GenericAPIView):
     def get(self, request, *args,  **kwargs):
