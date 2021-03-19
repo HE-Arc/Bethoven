2beec1941e92d2afaed13a20001154422f17d671
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-19 23:57:42 +0100

    add and register testcard vue

---
+++ Bethoven-app/bethoven/src/views/TestCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container fill-height fluid>
+    <div v-if="hasBets">
+      <div v-for="bet in bets" :key="bet.title">
+        <h1>{{ bet.title }}</h1>
+        <bet-card :title="bet.title" :description="bet.description"></bet-card>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import BetCard from "../components/BetCard.vue";
+import Api from "@/api/ApiRequester";
+import Vue from "vue";
+export default {
+  components: { BetCard },
+  name: "TestCard",
+  async beforeMount() {
+    this.bets = await Api.get("bets/?number=3&");
+  },
+  data() {
+    return {
+      bets: [],
+    };
+  },
+  computed: {
+    data(){
+      return this.bets;
+    },
+    hasBets() {
+      return this.bets != null && this.bets.length > 0;
+    },
+    lastID(){
+      return bets[bets.length - 1].id;
+    }
+  },
+};
+
+
+</script>
+
+<style>
+</style>
