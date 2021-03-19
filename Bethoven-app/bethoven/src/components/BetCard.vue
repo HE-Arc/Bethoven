18d772ae69c328781cf7854a6c978e069591a62c
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-19 23:57:48 +0100

    add betcard component

---
+++ Bethoven-app/bethoven/src/components/BetCard.vue
@@ -0,0 +1,28 @@
+<!-- TEMPLATE -->
+<template>
+  <div>
+    <v-card>
+        <v-card-title>{{title}}</v-card-title>
+        <h3>{{description}}</h3>
+    </v-card>
+  </div>
+</template>
+
+
+<!-- SCRIPT -->
+<script>
+import Vue from "vue";
+import Api from "@/api/ApiRequester";
+
+export default Vue.extend({
+  name: "BetCard",
+  props : {
+    title : "",
+    description : "",
+  },
+  computed: {
+  },
+  methods: {
+  },
+});
+</script>
