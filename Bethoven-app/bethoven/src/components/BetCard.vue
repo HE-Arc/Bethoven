0b14114f5ed3586cad35d8740a8a76d23f09a337
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-20 11:43:04 +0100

    add progress bar for teams

--- Bethoven-app/bethoven/src/components/BetCard.vue
+++ Bethoven-app/bethoven/src/components/BetCard.vue
@@ -1,20 +1,31 @@
 <!-- TEMPLATE -->
 <template>
   <div>
-    <v-card>
+    <v-card class="ma-1">
       <!-- Title and text -->
       <v-card-title>{{ bet.title }}</v-card-title>
       <v-card-text>{{ bet.description }}</v-card-text>
 
       <!-- 'bet' part with choices and bet buttons -->
+      <v-card-subtitle class="pa-0 text-center">
+        Total votes : {{ bet.bet_ratio.total }}
+      </v-card-subtitle>
+      <v-progress-linear
+        background-color="teamA"
+        color="teamB"
+        :value="bet.bet_ratio.ratio[0]"
+      ></v-progress-linear>
+
+      <!-- Tab -->
       <v-row no-gutters>
         <v-col>
           <!--Choice 1-->
-          <v-card  outlined tile>
-            <v-col class="text-right" >
+          <v-card outlined tile>
+            <v-col class="text-right">
               <v-card-subtitle class="pa-1">{{ bet.choice1 }}</v-card-subtitle>
-              <v-card-subtitle class="pa-1">{{ bet.bet_ratio[0] }} %</v-card-subtitle>
-
+              <v-card-subtitle class="pa-1"
+                >{{ bet.bet_ratio.ratio[0] }} %</v-card-subtitle
+              >
             </v-col>
           </v-card>
         </v-col>
@@ -23,7 +34,9 @@
           <v-card outlined tile>
             <v-col class="text-left">
               <v-card-subtitle class="pa-1">{{ bet.choice2 }}</v-card-subtitle>
-              <v-card-subtitle class="pa-1">{{ bet.bet_ratio[1] }} %</v-card-subtitle>
+              <v-card-subtitle class="pa-1"
+                >{{ bet.bet_ratio.ratio[1] }} %</v-card-subtitle
+              >
             </v-col>
           </v-card>
         </v-col>
