3044afb8286e1e7985f966e3bab98895c82e52c2
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-20 11:41:59 +0100

    add viewtify teamA/teamB

--- Bethoven-app/bethoven/src/plugins/vuetify.js
+++ Bethoven-app/bethoven/src/plugins/vuetify.js
@@ -1,7 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
+
 
 Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        
+        teamA: colors.blue.darken1,
+        teamB: colors.red.darken1,
+      },
+      dark: {
+      },
+    },
+  },
+})
 
 export default new Vuetify({
 });
