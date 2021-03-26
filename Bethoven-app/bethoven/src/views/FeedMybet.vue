cb2653692b768c556ace5994f68b4ea359894e15
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-26 14:56:29 +0100

    add feeds as views

---
+++ Bethoven-app/bethoven/src/views/FeedMybet.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container fill-height fluid>
+        <Feed initialQuery="bets/mybet">
+        </Feed>
+    </v-container>
+</template>
+
+<script>
+import Feed from "../components/Feed.vue";
+export default {
+    name: "FeedMybet",
+    components : {
+        Feed,
+    },
+}
+</script>
+
+<style>
+
+</style>
