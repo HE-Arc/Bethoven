d8854f83c858143e756af6f1b292a0ad0619655a
Maxime Welcklen <maxime.welcklen@he-arc.ch>
2021-03-27 09:06:18 +0100

    init search vue

---
+++ Bethoven-app/bethoven/src/views/Search.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+            <v-col cols=6>
+                <v-text-field
+                label="Search friend"
+                placeholder="Rajinth-xXx"
+                outlined
+                dense
+                append-icon="mdi-magnify"
+            ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
+            <v-col cols=8>
+                
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+//import Feed from "../components/Feed.vue";
+export default {
+    name: "Search",
+    components : {
+        //Feed,
+    },
+}
+
+</script>
+
+<style>
+
+</style>
